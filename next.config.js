6e5ba7d149430e965c6c709b69cdaae71c7b6a52
Byeong Gwan Seo <seo.jp.work@gmail.com>
2025-03-04 19:35:51 +0900

    deploy setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: "export",
+  basePath: "/react-practice-aribnb",
+  assetPrefix: "react-practice-aribnb",
 };
 
 module.exports = nextConfig;
